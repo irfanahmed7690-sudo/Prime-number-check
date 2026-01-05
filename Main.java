947f7bcca7d7455475d23d98dccbccb0320e5beb
Irfan Ahmed <17irfancodes.official@gmail.com>
2026-01-05 12:38:41 +0600

    Check if a number is prime or not

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+  public static void main(String[] args) {
+    int n = 5;
+    if(n <= 1) { //First condition to be a prime number
+    
+      System.out.println("Invalid number");
+      
+      return; //stop
+    
+    }
+    
+    int i = 2;
+    boolean isPrime = true;
+    
+    while(i <= n / 2) { // 2 <= 1, 3 <= 3
+    
+      if(n % i == 0) { // 7 % 2 != 0, 7 % 3 != 0
+      
+        isPrime = false;
+      
+      }
+      
+      i++;
+    
+    }
+    
+    if(isPrime) {
+    
+      System.out.println("Prime");
+    
+    } else {
+    
+      System.out.println("Not a prime");
+    
+    }
+    
+  }
+}
